1ccd003a7759c31b83e14577814bc2dd0c6b0f58
agrawalrajat310 <agrawalrajat03@outlook.com>
2022-11-13 00:59:00 +0530

    day-4 push

--- olx/src/Components/Footer.jsx
+++ olx/src/Components/Footer.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+// import React from 'react'
 
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+// const Footer = () => {
+//   return (
+//     <div>Footer</div>
+//   )
+// }
 
-export default Footer
+// export default Footer
+
+
+
