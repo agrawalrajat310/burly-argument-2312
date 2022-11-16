25c2d7e4bd966b0126f490ff8d9db64248f4baa5
agrawalrajat310 <agrawalrajat03@outlook.com>
2022-11-16 18:32:26 +0530

    c-5 eval

---
+++ olx/src/Routes/Data.jsx
@@ -0,0 +1,116 @@
+
+// "RAJAT"[
+    
+//   {
+//     img: "https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60",
+//     title: "Plot-for-sale-near-jawolamukhi-ammavaru-temp",
+//     price: "RS 1000"
+//   },
+
+//   {
+//     img: "https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60",
+//     title: "Plot-for-sale-near-jawolamukhi-ammavaru-temp",
+//     price: "RS 1000"
+//   },
+
+//   {
+//     img: "https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60",
+//     title: "Plot-for-sale-near-jawolamukhi-ammavaru-temp",
+//     price: "RS 1000"
+//   },
+
+//   {
+//     img: "https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60",
+//     title: "Plot-for-sale-near-jawolamukhi-ammavaru-temp",
+//     price: "RS 1000"
+//   },
+
+//   {
+//     img: "https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60",
+//     title: "Plot-for-sale-near-jawolamukhi-ammavaru-temp",
+//     price: "RS 1000"
+//   },
+
+//   {
+//     img: "https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60",
+//     title: "Plot-for-sale-near-jawolamukhi-ammavaru-temp",
+//     price: "RS 1000"
+//   },
+
+//   {
+//     img: "https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60",
+//     title: "Plot-for-sale-near-jawolamukhi-ammavaru-temp",
+//     price: "RS 1000"
+//   },
+
+//   {
+//     img: "https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60",
+//     title: "Plot-for-sale-near-jawolamukhi-ammavaru-temp",
+//     price: "RS 1000"
+//   },
+
+//   {
+//     img: "https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60",
+//     title: "Plot-for-sale-near-jawolamukhi-ammavaru-temp",
+//     price: "RS 1000"
+//   },
+//   {
+//     img: "https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60",
+//     title: "Plot-for-sale-near-jawolamukhi-ammavaru-temp",
+//     price: "RS 1000"
+//   },
+
+//   {
+//     img: "https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60",
+//     title: "Plot-for-sale-near-jawolamukhi-ammavaru-temp",
+//     price: "RS 1000"
+//   },
+
+//   {
+//     img: "https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60",
+//     title: "Plot-for-sale-near-jawolamukhi-ammavaru-temp",
+//     price: "RS 1000"
+//   },
+
+//   {
+//     img: "https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60",
+//     title: "Plot-for-sale-near-jawolamukhi-ammavaru-temp",
+//     price: "RS 1000"
+//   },
+
+//   {
+//     img: "https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60",
+//     title: "Plot-for-sale-near-jawolamukhi-ammavaru-temp",
+//     price: "RS 1000"
+//   },
+
+//   {
+//     img: "https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60",
+//     title: "Plot-for-sale-near-jawolamukhi-ammavaru-temp",
+//     price: "RS 1000"
+//   },
+
+//   {
+//     img: "https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60",
+//     title: "Plot-for-sale-near-jawolamukhi-ammavaru-temp",
+//     price: "RS 1000"
+//   },
+
+//   {
+//     img: "https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60",
+//     title: "Plot-for-sale-near-jawolamukhi-ammavaru-temp",
+//     price: "RS 1000"
+//   },
+
+//   {
+//     img: "https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60",
+//     title: "Plot-for-sale-near-jawolamukhi-ammavaru-temp",
+//     price: "RS 1000"
+//   },
+
+//   {
+//     img: "https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60",
+//     title: "Plot-for-sale-near-jawolamukhi-ammavaru-temp",
+//     price: "RS 1000"
+//   }
+// ];
--- olx/src/Routes/Home.jsx
+++ olx/src/Routes/Home.jsx
@@ -1,63 +1,229 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 
-import AddItem from "../Components/AddItem";
+import { grid } from "@chakra-ui/react";
+import { resolvePath } from "react-router-dom";
+
+// import AddItem from "../Components/AddItem";
+
+// function HomePage() {
+//   const [list, setList] = useState([]);
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     if (loading) {
+//       let _list = [
+
+//       ];
+
+//       for (let i = 0; i < 18; i++) {
+//         _list.push(<AddItem placeholder={true} />);
+//       }
+
+//       setList(_list);
+//     }
+//   }, list);
+
+//   return (
+//     <>
+//       <div className="home-page flex">
+//         <div className="add-list flex">{list}</div>
+//       </div>
+
+//       <button className="load-more fontb c333 anim s20">Load More</button>
+//       <div className="app-ribbon flex aic">
+//         <div className="img">
+//           <img
+//             className="bl"
+//             src="https://statics.olx.in/external/base/img/phone-app.webp"
+//             alt="ribbon image"
+//           />
+//         </div>
+
+//         <div className="meta">
+//           <h2 className="title fontb s30 color">TRY THE OLX APP</h2>
+//           <h2 className="slogan font s18 color">
+//             Buy, sell and find just about anything using the app on your mobile.
+//           </h2>
+//         </div>
+//         <div className="links">
+//           <h2 className="title fontb s20 color">GET YOUR APP TODAY</h2>
+//           <div className="flex as">
+//             <a href="#" className="noul bl">
+//               <img
+//                 src="https://statics.olx.in/external/base/img/appstore_2x.png"
+//                 alt="appstore image"
+//               />
+//             </a>
+//             <a href="#" className="noul bl">
+//               <img
+//                 src="https://statics.olx.in/external/base/img/playstore_2x.png"
+//                 alt="playstore image"
+//               />
+//             </a>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default HomePage;
 
 function HomePage() {
-  const [list, setList] = useState([]);
-  const [loading, setLoading] = useState(true);
+  // const style = {
 
-  useEffect(() => {
-    if (loading) {
-      let _list = [];
+  //   display:'grid',
+  //   grid-template-columns: "repeat(4, 1fr)"
 
-      for (let i = 0; i < 18; i++) {
-        _list.push(<AddItem placeholder={true} />);
-      }
-
-      setList(_list);
-    }
-  }, list);
-
+  // }
   return (
-    <>
-      <div className="home-page flex">
-        <div className="add-list flex">{list}</div>
+    <div id="cont">
+      <div>
+        <img
+          src="https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60"
+          alt="error"
+        />
+        <h2>Plot-for-sale-near-jawolamukhi</h2>
+        <p>RS 1000</p>
       </div>
-      <button className="load-more fontb c333 anim s20">Load More</button>
-      <div className="app-ribbon flex aic">
-        <div className="img">
-          <img
-            className="bl"
-            src="https://statics.olx.in/external/base/img/phone-app.webp"
-            alt="ribbon image"
-          />
-        </div>
 
-        <div className="meta">
-          <h2 className="title fontb s30 color">TRY THE OLX APP</h2>
-          <h2 className="slogan font s18 color">
-            Buy, sell and find just about anything using the app on your mobile.
-          </h2>
-        </div>
-        <div className="links">
-          <h2 className="title fontb s20 color">GET YOUR APP TODAY</h2>
-          <div className="flex as">
-            <a href="#" className="noul bl">
-              <img
-                src="https://statics.olx.in/external/base/img/appstore_2x.png"
-                alt="appstore image"
-              />
-            </a>
-            <a href="#" className="noul bl">
-              <img
-                src="https://statics.olx.in/external/base/img/playstore_2x.png"
-                alt="playstore image"
-              />
-            </a>
-          </div>
-        </div>
+      <div>
+        <img
+          src="https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60"
+          alt="error"
+        />
+        <h2>Plot-for-sale-near-jawolamukhi</h2>
+        <p>RS 1000</p>
       </div>
-    </>
+      <div>
+        <img
+          src="https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60"
+          alt="error"
+        />
+        <h2>Plot-for-sale-near-jawolamukhi</h2>
+        <p>RS 1000</p>
+      </div>
+      <div>
+        <img
+          src="https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60"
+          alt="error"
+        />
+        <h2>Plot-for-sale-near-jawolamukhi</h2>
+        <p>RS 1000</p>
+      </div>
+      <div>
+        <img
+          src="https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60"
+          alt="error"
+        />
+        <h2>Plot-for-sale-near-jawolamukhi</h2>
+        <p>RS 1000</p>
+      </div>
+      <div>
+        <img
+          src="https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60"
+          alt="error"
+        />
+        <h2>Plot-for-sale-near-jawolamukhi</h2>
+        <p>RS 1000</p>
+      </div>
+      <div>
+        <img
+          src="https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60"
+          alt="error"
+        />
+        <h2>Plot-for-sale-near-jawolamukhi</h2>
+        <p>RS 1000</p>
+      </div>
+      <div>
+        <img
+          src="https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60"
+          alt="error"
+        />
+        <h2>Plot-for-sale-near-jawolamukhi</h2>
+        <p>RS 1000</p>
+      </div>
+      <div>
+        <img
+          src="https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60"
+          alt="error"
+        />
+        <h2>Plot-for-sale-near-jawolamukhi</h2>
+        <p>RS 1000</p>
+      </div>
+      <div>
+        <img
+          src="https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60"
+          alt="error"
+        />
+        <h2>Plot-for-sale-near-jawolamukhi</h2>
+        <p>RS 1000</p>
+      </div>
+      <div>
+        <img
+          src="https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60"
+          alt="error"
+        />
+        <h2>Plot-for-sale-near-jawolamukhi</h2>
+        <p>RS 1000</p>
+      </div>
+      <div>
+        <img
+          src="https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60"
+          alt="error"
+        />
+        <h2>Plot-for-sale-near-jawolamukhi</h2>
+        <p>RS 1000</p>
+      </div>
+      <div>
+        <img
+          src="https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60"
+          alt="error"
+        />
+        <h2>Plot-for-sale-near-jawolamukhi</h2>
+        <p>RS 1000</p>
+      </div>
+      <div>
+        <img
+          src="https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60"
+          alt="error"
+        />
+        <h2>Plot-for-sale-near-jawolamukhi</h2>
+        <p>RS 1000</p>
+      </div>
+      <div>
+        <img
+          src="https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60"
+          alt="error"
+        />
+        <h2>Plot-for-sale-near-jawolamukhi</h2>
+        <p>RS 1000</p>
+      </div>
+      <div>
+        <img
+          src="https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60"
+          alt="error"
+        />
+        <h2>Plot-for-sale-near-jawolamukhi</h2>
+        <p>RS 1000</p>
+      </div>
+      <div>
+        <img
+          src="https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60"
+          alt="error"
+        />
+        <h2>Plot-for-sale-near-jawolamukhi</h2>
+        <p>RS 1000</p>
+      </div>
+      <div>
+        <img
+          src="https://apollo-singapore.akamaized.net/v1/files/htzkzeri71bj2-IN/image;s=300x600;q=60"
+          alt="error"
+        />
+        <h2>Plot-for-sale-near-jawolamukhi</h2>
+        <p>RS 1000</p>
+      </div>
+    </div>
   );
 }
 
